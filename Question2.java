5e93be47ec369e30949f245ae09ebd4d9701aedb
Klot-Rap <92530012+Klot-Rap@users.noreply.github.com>
2022-04-19 15:19:57 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -5,10 +5,10 @@ public class Question2
   public static void main(String[] args)
   {
     Scanner scan = new Scanner(System.in);
-    System.out.print("What is your current weight(kg): ");
+    //System.out.print("What is your current weight(kg): ");
     double weight = scan.nextDouble();
 
-    System.out.print("What is your current height(m): ");
+    //System.out.print("What is your current height(m): ");
     double height = scan.nextDouble();
 
     double BMI = weight / (height * height);
